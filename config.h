d421d2d03db8fafb961842a27f7b0cffa41d69d4
oversys <hani.alsayani11@gmail.com>
2023-03-23 12:51:47 +0400

    Add files via upload

--- config.h
+++ config.h
@@ -49,7 +49,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb" };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "brave", NULL };
 
