fe0709ed94b68ecb2f64399f490de3dd9b66c30c
oversys <hani.alsayani11@gmail.com>
2024-02-01 21:07:14 +0400

    Add files via upload

--- dwm.c
+++ dwm.c
@@ -738,6 +738,9 @@ drawbar(Monitor *m)
 		else if (occ & 1 << i) tagscheme = SchemeNorm;
 		drw_setscheme(drw, scheme[tagscheme]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
+		if (ulineall || m->tagset[m->seltags] & 1 << i) /* if there are conflicts, just move these lines directly underneath both 'drw_setscheme' and 'drw_text' :) */
+			drw_rect(drw, x + ulinepad, bh - ulinestroke - ulinevoffset, w - (ulinepad * 2), ulinestroke, 1, 0);
+			
 		x += w;
 	}
 	
@@ -746,7 +749,7 @@ drawbar(Monitor *m)
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		if (occ & 1 << i)
+				if (occ & 1 << i)
 			drw_rect(drw, x + boxs, boxs, boxw, boxw,
 				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 				urg & 1 << i);
