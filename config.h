754fbab2fddd6f387c2f6af1db8691c1c42fea3a
oversys <hani.alsayani11@gmail.com>
2024-02-03 16:55:39 +0400

    Centered dmenu

--- config.h
+++ config.h
@@ -53,7 +53,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-c", "-l", "10", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[]  = { "brave", NULL };
 static const char *screenshot[] = {"flameshot", "gui", NULL};
